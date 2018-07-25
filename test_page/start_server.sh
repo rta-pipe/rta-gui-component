abf70a53be8e566f4779de9bbece636245e5f256
Leonardo Baroncelli <leonardo.baroncelli26@gmail.com>
2018-07-25 23:29:48 +0200

    added light curve

--- test_page/start_server.sh
+++ test_page/start_server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cp ../light-curve/light-curve.html components/
+. ../../nodejs-v8.11.3/load_node_module.sh
 
+cp ../components/plotly_components/light-curve/light-curve.html components/
 
+printf '\nExecute in a local shell:\nssh -N -L 8600:localhost:3000 cta@agilepipedev.iasfbo.inaf.it\n\n'
 
 node index.js
