33258d9708834f3433baab567f37a082eba7b7e0
Leofaber <leonardo.baroncelli26@gmail.com>
2018-07-30 22:57:22 +0200

    spectral light curve

--- test_page/index.js
+++ test_page/index.js
@@ -3,7 +3,13 @@ var app = express();
 var path = require('path');
 
 app.use('/static', express.static(path.join(__dirname, '/examples')))
+app.use("/imports", express.static(path.join(__dirname, '/imports')));
+app.use("/styles", express.static(path.join(__dirname, '/styles')));
+
 app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')))
+app.use(express.static(path.join(__dirname, '/bower_components/MathJax')))
+
+
 app.use('/components', express.static(path.join(__dirname, '/components')))
 
 app.get('/', function (req, res) {
--- test_page/start_server.sh
+++ test_page/start_server.sh
@@ -2,7 +2,16 @@
 
 . ../../nodejs-v8.11.3/load_node_module.sh
 
+# Styles
+cp -r ../components/plotly_components/styles/plotly-style.html styles/
+
+# Components
 cp ../components/plotly_components/light-curve.html components/
+cp ../components/plotly_components/spectral-light-curve.html components/
+
+# Imports
+cp -r ../components/plotly_components/imports ./
+
 
 printf '\nExecute in a local shell:\nssh -N -L 8600:localhost:3000 cta@agilepipedev.iasfbo.inaf.it\n\n'
 
